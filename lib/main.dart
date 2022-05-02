4b374b053e959c75af22a6005cabb76ee5cf7259
Terralien-jp <terralienjp@gmail.com>
2022-05-02 16:02:12 +0900

    fix ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -71,14 +71,40 @@ class TodoAddPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text('リスト追加画面（クリックで戻る）'),
+        appBar: AppBar(
+          title: Text('リスト追加'),
         ),
-      ),
-    );
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(),
+              const SizedBox(height: 8),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  // color: Colors.blue,
+                  onPressed: () {},
+                  child: Text('リスト追加', style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Container(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('キャンセル'),
+                  ))
+            ])
+        // body: Center(
+        //   child: TextButton(
+        //     onPressed: () {
+        //       Navigator.of(context).pop();
+        //     },
+        //     child: Text('リスト追加画面（クリックで戻る）'),
+        //   ),
+        // ),
+        );
   }
 }
